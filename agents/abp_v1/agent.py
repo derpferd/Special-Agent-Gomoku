875cf16d57c0fcb7874d0beaeb0fe2d06f6554bd
Vaclav Hasenohrl <hasen011@d.umn.edu>
2018-02-18 19:25:15 -0600

    alpha beta pruning
    node class, checking methods

--- agents/abp_v1/agent.py
+++ agents/abp_v1/agent.py
@@ -15,24 +15,174 @@ class Node:
         self.o = opponent
         self.m = move  # whose move it is
         self.r = 0  # reward
+        self.threats = {}
 
+    # returns the number of (rows/columns)^2 - 1
     def get_board_size(self):
         return max(max(self.p, self.o), self.f)
 
-    def generate_threats(self):
+    def check_rows(self, curr, stack):
         row_length = sqrt(self.get_board_size() + 1);
+        threat = {curr}
         # check rows
-        s = 0
+        # left
+        count = 1
+        moves = 1
+        while (curr - moves) % row_length == curr % row_length:
+            if curr - moves in self.o:
+                threat.append(curr - moves)
+                moves += 1
+                count += 1
+            elif curr - moves in self.f:
+                threat.append(curr - moves)
+                break
+            else:
+                break
+        # right
+        moves = 1
+        while (curr + moves) % row_length == curr % row_length:
+            if curr + moves in self.o:
+                threat.append(curr - moves)
+                moves += 1
+                count += 1
+            elif curr + moves in self.f:
+                threat.append(curr - moves)
+                break
+            else:
+                break
 
+        if count == 3 and len(threat) == 5:
+            self.threats.append(threat)
+            stack = threat ^ stack
+        elif count == 4 and len(threat) == 5:
+            self.threats.append(threat)
+            stack = threat ^ stack
+
+        return stack
+
+    def check_columns(self, curr, stack):
+        row_length = sqrt(self.get_board_size() + 1);
+        threat = {curr}
         # check columns
-        s = 0
+        # down
+        count = 1
+        moves = 1
+        while curr - moves * row_length >= 0:
+            if curr - moves * row_length in self.o:
+                threat.append(curr - moves * row_length)
+                moves += 1
+                count += 1
+            elif curr - moves * row_length in self.f:
+                threat.append(curr - moves * row_length)
+                break
+            else:
+                break
+        # up
+        moves = 1
+        while curr + moves * row_length < row_length ^ 2:
+            if curr + moves * row_length in self.o:
+                threat.append(curr - moves * row_length)
+                moves += 1
+                count += 1
+            elif curr + moves * row_length in self.f:
+                threat.append(curr - moves * row_length)
+                break
+            else:
+                break
 
-        # check left diagonals
-        s = self.get_board_size() + 1 - row_length
+        if count == 3 and len(threat) == 5:
+            self.threats.append(threat)
+            stack = threat ^ stack
+        elif count == 4 and len(threat) == 5:
+            self.threats.append(threat)
+            stack = threat ^ stack
 
-        # check right diagonals
-        s = self.get_board_size()
-        for r in range()
+        return stack
+
+    def check_diag(self, curr, stack):
+        row_length = sqrt(self.get_board_size() + 1);
+        threat = {curr}
+        r = curr % row_length
+        c = curr - r * row_length
+        # check left to right
+        # down
+        count = 1
+        moves = 1
+        while r - moves >= 0 and c + moves < row_length:
+            if (r - moves) * row_length + (c + moves) in self.o:
+                threat.append((r - moves) * row_length + (c + moves))
+                moves += 1
+                count += 1
+            elif (r - moves) * row_length + (c + moves) in self.f:
+                threat.append((r - moves) * row_length + (c + moves))
+                break
+            else:
+                break
+        # up
+        moves = 1
+        while r + moves < row_length and c - moves >= 0:
+            if (r + moves) * row_length + (c - moves) in self.o:
+                threat.append((r + moves) * row_length + (c - moves))
+                moves += 1
+                count += 1
+            elif (r + moves) * row_length + (c - moves) in self.f:
+                threat.append((r + moves) * row_length + (c - moves))
+                break
+            else:
+                break
+
+        if count == 3 and len(threat) == 5:
+            self.threats.append(threat)
+            stack = threat ^ stack
+        elif count == 4 and len(threat) == 5:
+            self.threats.append(threat)
+            stack = threat ^ stack
+
+        threat = {curr}
+        # check right to left
+        # down
+        count = 1
+        moves = 1
+        while r - moves >= 0 and c - moves >= 0:
+            if (r - moves) * row_length + (c - moves) in self.o:
+                threat.append((r - moves) * row_length + (c - moves))
+                moves += 1
+                count += 1
+            elif (r - moves) * row_length + (c - moves) in self.f:
+                threat.append((r - moves) * row_length + (c - moves))
+                break
+            else:
+                break
+        # up
+        moves = 1
+        while r + moves < row_length and c + moves < row_length:
+            if (r + moves) * row_length + (c + moves) in self.o:
+                threat.append((r + moves) * row_length + (c + moves))
+                moves += 1
+                count += 1
+            elif (r + moves) * row_length + (c + moves) in self.f:
+                threat.append((r + moves) * row_length + (c + moves))
+                break
+            else:
+                break
+
+        if count == 3 and len(threat) == 5:
+            self.threats.append(threat)
+            stack = threat ^ stack
+        elif count == 4 and len(threat) == 5:
+            self.threats.append(threat)
+            stack = threat ^ stack
+
+        return stack
+
+    def generate_threats(self):
+        stack = self.o[:]
+        while len(stack) != 0:
+            curr = stack.pop()
+            stack = self.check_rows(curr, stack)
+            stack = self.check_columns(curr, stack)
+            stack = self.check_diag(curr, stack)
+
 
 class ABP(Agent):
     def start_game(self, action_space: List[int]) -> None:
@@ -42,4 +192,6 @@ class ABP(Agent):
         pass
 
     def move(self, state: GomokuState) -> int:
+        root = Node(None, None, None, None, -1)
+
         return choice(list(state.board.valid_actions))
