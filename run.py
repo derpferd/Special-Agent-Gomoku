7bcdf579a548751cbe06f0443bb1d427d82a2207
Jonathan Beaulieu <123.jonathan@gmail.com>
2018-03-07 11:55:33 -0600

    Bug fix for Dennis :)

--- run.py
+++ run.py
@@ -45,9 +45,9 @@ def play_game(env_gen: Callable[[], GomokuEnv], agents: List[Agent], seed: int=N
         _, reward, done, _ = env.step(action)
 
         if done:
-            cur_agent.end_game(reward == 1)
-            o_agent.end_game(reward != 1)
-            if reward != 1:
+            cur_agent.end_game(reward == -1)
+            o_agent.end_game(reward == 1)
+            if reward == -1:
                 winner = cur_agent.config.id
             else:
                 winner = o_agent.config.id
