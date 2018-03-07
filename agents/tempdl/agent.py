e752c2872bb8e94ba693088caf867c6744ee341e
Dennis Asamoah-Owusu <dennisasamoahowusu@gmail.com>
2018-03-07 10:01:33 -0600

    updated tempdl agent

--- agents/tempdl/agent.py
+++ agents/tempdl/agent.py
@@ -1,26 +1,33 @@
 # Author: Jonathan Beaulieu
-from random import choice
-from typing import List
 from random import random
+from typing import List
+
+from gym_gomoku import GomokuState
 
 from .. import Agent
-from gym_gomoku import GomokuState
+from . import mlp
 
 
 class Tempdl(Agent):
+
     def start_game(self, action_space: List[int]) -> None:
         print('Tempdl agent starting:')
+        self.mlp = mlp.Mlp()
 
     def end_game(self, won: bool) -> None:
         print('Game ended')
-        if won:
+        if won == True:
             print('I won!')
         else:
-            print('I lost!')
+            print('I lost!!')
 
     def move(self, state: GomokuState) -> int:
         # random = choice(list(state.board.valid_actions))
         # return random
+
+        #print('============================================================\n')
+        #print(state.board.board_state)
+        #print('============================================================\n')
 
         maxvalue = -1
         selected_action = -1
@@ -32,4 +39,5 @@ class Tempdl(Agent):
         return selected_action
 
     def evaluate(self, action, state):
-        return random()
+        #set state to state after action
+        return self.mlp.evaluate(state)
--- agents/tempdl/mlp.py
+++ agents/tempdl/mlp.py
@@ -2,9 +2,27 @@
 ###Each where each index is a cell and the value of the index is
 ###based on what is in the cell, black, white, empty
 ###Intialize random weight vector and multiply to get the value of the state
+
+from random import random
+
+
 class Mlp:
     def __init__(self):
         self.layers = []
+        #load layer weights from file
 
-    def predict(self):
-        print("Predict called")
+    def evaluate(self, state):
+        vector = self.vector(state)
+        return random()
+
+    def vector(self, state):
+        vector = []
+        for row in state.board.board_state:
+            for col in row:
+                vector.append(col)
+
+        #print("=============================\n")
+        #print(vector)
+        #print("==============================\n")
+
+        return vector
