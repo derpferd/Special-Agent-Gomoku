5d4f8fbee975c9f1e740d15ffdaf2b9629ee5b5b
JDFenix57 <recursivemoose@gmail.com>
2018-04-05 19:04:45 -0500

    Tempdl update

--- agents/tempdl/model/perceptron.py
+++ agents/tempdl/model/perceptron.py
@@ -1,11 +1,14 @@
 import numpy as np
+from random import random
 
 
 class Perceptron:
     def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
-        self.output = []
+        self.prevoutput = 0
+        self.curoutput = 0
+        self.gradients = [[]]
 
     def __repr__(self):
         s = "\n================== Perceptron ======================"
@@ -19,7 +22,30 @@ class Perceptron:
         if len(input_vector) != len(self.weights):
             raise ValueError("dimensions for input and weights don't match")
         output = np.dot(input_vector, self.weights) + self.bias
-        return np.maximum(output, 0)
+        self.prevoutput = self.curoutput
+        self.add_gradient()
+        self.curoutput = np.maximum(output, 0)
+        self.updateweights()
+        return self.curoutput
 
     def updateweights(self):
-        pass
+        new_weights = []
+        alpha = 0.7
+
+        for i in len(self.weights):
+            new_weights[i] = self.weights[i] + (alpha * (self.curoutput - self.prevoutput) * self.gradient_sum()[i])
+
+    def add_gradient(self):
+        new_gradient = self.calc_gradient()
+        self.gradients.append(new_gradient)
+
+    def calc_gradient(self):
+        return [random() for i in len(self.weights)]
+
+    def gradient_sum(self):
+        arr = np.zeros(len(self.weights))
+        for i in len(self.gradients):
+            arr = np.add(arr, np.asarray(self.gradients[i]))
+        return arr
+
+
